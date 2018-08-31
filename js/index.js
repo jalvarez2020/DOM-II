439c969edaac70cfd78d249bc2a26451f92866c3
jalvarez2020 <javieralvarez82@live.com>
2018-08-30 22:52:54 -0700

    first commit completed 2/10 listeners.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 // Your code goes here
+const navLinks = document.querySelectorAll('.nav a');
+
+const navHoverHandler = function (event) {
+  if (event.target.style.color = ' ') {
+
+    event.target.style.color = 'red ';
+  } else {
+    event.target.style.color = 'black '
+  }
+};
+
+navLinks.forEach((hover) => {
+  hover.addEventListener('mouseover', navHoverHandler);
+})
+
+function display(event) {
+  event.target.style.filter = 'grayscale(100%)
+}
+
+const introLogo = document.querySelector(".intro img");
+introLogo.addEventListener('wheel', display);
+
+
+let headerLogo = document.querySelector('.logo-heading');
+headerLogo.forEachd
+
