aa4025d2475767b341c0a26648f7b0898647a906
jalvarez2020 <javieralvarez82@live.com>
2018-09-01 08:40:47 -0700

    completed 8 of 10 inputs.

--- js/index.js
+++ js/index.js
@@ -1,27 +1,70 @@
 // Your code goes here
 const navLinks = document.querySelectorAll('.nav a');
+const introLogo = document.querySelector('.intro img');
+const headerLogo = document.querySelector('.logo-heading');
+const textContent = document.querySelectorAll('.text-content');
+const imgContent = document.querySelectorAll('.img-content');
+const button = document.querySelectorAll('.btn');
+const paragraph = document.querySelectorAll('p');
+const pHeader2 = document.querySelectorAll('h2');
+const pHeader4 = document.querySelectorAll('h4');
+
+
+
 
 const navHoverHandler = function (event) {
-  if (event.target.style.color = ' ') {
-
-    event.target.style.color = 'red ';
-  } else {
-    event.target.style.color = 'black '
-  }
-};
-
-navLinks.forEach((hover) => {
-  hover.addEventListener('mouseover', navHoverHandler);
-})
-
-function display(event) {
-  event.target.style.filter = 'grayscale(100%)
+  event.target.style.color = 'red';
 }
 
-const introLogo = document.querySelector(".intro img");
+navLinks.forEach((hover) => {
+  hover.addEventListener('mouseover', navHoverHandler)
+})
+
+const display = function (event) {
+  event.target.style.filter = 'grayscale(100%)';
+}
+
 introLogo.addEventListener('wheel', display);
 
+const click = function (event) {
+  window.open('https://en.wikipedia.org/wiki/School_bus')
+}
 
-let headerLogo = document.querySelector('.logo-heading');
-headerLogo.forEachd
+headerLogo.addEventListener('click', click)
 
+const textShadow = function (event) {
+  event.target.style.textShadow = '10px 10px gray';
+}
+
+textContent.forEach((shadow) => {
+  shadow.addEventListener('dblclick', textShadow);
+});
+
+const shadowBox = function (event) {
+  event.target.style.boxShadow = ('5px 5px gray');
+}
+imgContent.forEach((boxShadow) => {
+  boxShadow.addEventListener('mouseover', shadowBox);
+})
+
+const btnScale = function (event) {
+  event.target.style.transform = 'scale(2, 3)'
+}
+
+button.forEach((sizeBtn) => {
+  sizeBtn.addEventListener('mouseover', btnScale);
+})
+
+const paragraphDisplay = function (event) {
+  event.target.style.display = 'none';
+}
+paragraph.forEach((hide) => {
+  hide.addEventListener('mousemove', paragraphDisplay)
+})
+
+const pHeaders = function (event) {
+  event.target.style.fontSize = '6rem';
+}
+pHeader2.forEach((resize) => {
+  resize.addEventListener('mouseout', pHeaders)
+});
